58170bc420c9559a371a3f77c1437374a4e8ca30
Bassem Yacoube <bassemgeorgi@gmail.com>
2020-01-06 18:07:52 -0800

    Add files via upload

---
+++ model/ddpg/ddpg.py
@@ -0,0 +1,210 @@
+"""
+The deep deterministic policy gradient model. Contains main training loop and deployment
+"""
+from __future__ import print_function
+
+import os
+import traceback
+import json
+import numpy as np
+import tensorflow as tf
+
+from .replay_buffer import ReplayBuffer
+from ..base_model import BaseModel
+
+
+def build_summaries():
+    episode_reward = tf.Variable(0.)
+    tf.summary.scalar("Reward", episode_reward)
+    episode_ave_max_q = tf.Variable(0.)
+    tf.summary.scalar("Qmax_Value", episode_ave_max_q)
+
+    summary_vars = [episode_reward, episode_ave_max_q]
+    summary_ops = tf.summary.merge_all()
+
+    return summary_ops, summary_vars
+
+
+class DDPG(BaseModel):
+    def __init__(self, env, sess, actor, critic, actor_noise, obs_normalizer=None, action_processor=None,
+                 config_file='config/default.json',
+                 model_save_path='weights/ddpg/ddpg.ckpt', summary_path='results/ddpg/'):
+        with open(config_file) as f:
+            self.config = json.load(f)
+        assert self.config != None, "Can't load config file"
+        np.random.seed(self.config['seed'])
+        if env:
+            env.seed(self.config['seed'])
+        self.model_save_path = model_save_path
+        self.summary_path = summary_path
+        self.sess = sess
+        # if env is None, then DDPG just predicts
+        self.env = env
+        self.actor = actor
+        self.critic = critic
+        self.actor_noise = actor_noise
+        self.obs_normalizer = obs_normalizer
+        self.action_processor = action_processor
+        self.summary_ops, self.summary_vars = build_summaries()
+
+    def initialize(self, load_weights=True, verbose=True):
+        """ Load training history from path. To be add feature to just load weights, not training states
+
+        """
+        if load_weights:
+            try:
+                variables = tf.global_variables()
+                param_dict = {}
+                saver = tf.train.Saver()
+                saver.restore(self.sess, self.model_save_path)
+                for var in variables:
+                    var_name = var.name[:-2]
+                    if verbose:
+                        print('Loading {} from checkpoint. Name: {}'.format(var.name, var_name))
+                    param_dict[var_name] = var
+            except:
+                traceback.print_exc()
+                print('Build model from scratch')
+                self.sess.run(tf.global_variables_initializer())
+        else:
+            print('Build model from scratch')
+            self.sess.run(tf.global_variables_initializer())
+
+    def train(self, save_every_episode=1, verbose=True, debug=False):
+        """ Must already call intialize
+
+        Args:
+            save_every_episode:
+            print_every_step:
+            verbose:
+            debug:
+
+        Returns:
+
+        """
+        writer = tf.summary.FileWriter(self.summary_path, self.sess.graph)
+        print('inside DDPG train')
+        self.actor.update_target_network()
+        self.critic.update_target_network()
+
+        np.random.seed(self.config['seed'])
+        num_episode = self.config['episode']
+        batch_size = self.config['batch size']
+        gamma = self.config['gamma']
+        self.buffer = ReplayBuffer(self.config['buffer size'])
+
+        # main training loop
+        for i in range(num_episode):
+            if verbose and debug:
+                print("Episode: " + str(i) + " Replay Buffer " + str(self.buffer.count()))
+
+            previous_observation = self.env.reset()
+            if self.obs_normalizer:
+                previous_observation = self.obs_normalizer(previous_observation)
+
+            ep_reward = 0
+            ep_ave_max_q = 0
+            # keeps sampling until done
+            for j in range(self.config['max step']):
+                action = self.actor.predict(np.expand_dims(previous_observation, axis=0)).squeeze(
+                    axis=0) + self.actor_noise()
+
+                if self.action_processor:
+                    action_take = self.action_processor(action)
+                else:
+                    action_take = action
+                # step forward
+                observation, reward, done, _ = self.env.step(action_take)
+
+                if self.obs_normalizer:
+                    observation = self.obs_normalizer(observation)
+
+                # add to buffer
+                self.buffer.add(previous_observation, action, reward, done, observation)
+
+                if self.buffer.size() >= batch_size:
+                    # batch update
+                    s_batch, a_batch, r_batch, t_batch, s2_batch = self.buffer.sample_batch(batch_size)
+                    # Calculate targets
+                    target_q = self.critic.predict_target(s2_batch, self.actor.predict_target(s2_batch))
+
+                    y_i = []
+                    for k in range(batch_size):
+                        if t_batch[k]:
+                            y_i.append(r_batch[k])
+                        else:
+                            y_i.append(r_batch[k] + gamma * target_q[k])
+
+                    # Update the critic given the targets
+                    predicted_q_value, _ = self.critic.train(
+                        s_batch, a_batch, np.reshape(y_i, (batch_size, 1)))
+
+                    ep_ave_max_q += np.amax(predicted_q_value)
+
+                    # Update the actor policy using the sampled gradient
+                    a_outs = self.actor.predict(s_batch)
+                    grads = self.critic.action_gradients(s_batch, a_outs)
+                    self.actor.train(s_batch, grads[0])
+
+                    # Update target networks
+                    self.actor.update_target_network()
+                    self.critic.update_target_network()
+
+                ep_reward += reward
+                previous_observation = observation
+
+                if done or j == self.config['max step'] - 1:
+                    summary_str = self.sess.run(self.summary_ops, feed_dict={
+                        self.summary_vars[0]: ep_reward,
+                        self.summary_vars[1]: ep_ave_max_q / float(j)
+                    })
+
+                    writer.add_summary(summary_str, i)
+                    writer.flush()
+
+                    print('Episode: {:d}, Reward: {:.2f}, Qmax: {:.4f}'.format(i, ep_reward, (ep_ave_max_q / float(j))))
+                    break
+        print('save model.')
+        self.save_model(verbose=True)
+        print('Finish.')
+
+    def predict(self, observation):
+        """ predict the next action using actor model, only used in deploy.
+            Can be used in multiple environments.
+
+        Args:
+            observation: (batch_size, num_stocks + 1, window_length)
+
+        Returns: action array with shape (batch_size, num_stocks + 1)
+
+        """
+        if self.obs_normalizer:
+            observation = self.obs_normalizer(observation)
+        action = self.actor.predict(observation)
+        if self.action_processor:
+            action = self.action_processor(action)
+        return action
+
+    def predict_single(self, observation):
+        """ Predict the action of a single observation
+
+        Args:
+            observation: (num_stocks + 1, window_length)
+
+        Returns: a single action array with shape (num_stocks + 1,)
+
+        """
+        if self.obs_normalizer:
+            observation = self.obs_normalizer(observation)
+        action = self.actor.predict(np.expand_dims(observation, axis=0)).squeeze(axis=0)
+        if self.action_processor:
+            action = self.action_processor(action)
+        return action
+
+    def save_model(self, verbose=False):
+        if not os.path.exists(self.model_save_path):
+            os.makedirs(self.model_save_path, exist_ok=True)
+
+        saver = tf.train.Saver()
+        model_path = saver.save(self.sess, self.model_save_path)
+        print("Model saved in %s" % model_path)
